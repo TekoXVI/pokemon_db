3a01faa65fd53a06a7922e93986a34ce170fe43d
Ian <tekoxvi@gmail.com>
2022-04-11 16:44:25 -0600

    first commit

---
+++ schema.sql
@@ -0,0 +1,44 @@
+ 
+ 
+ create table 'pokemon' (
+ 	'id' integer primary key,
+	'name' text not null,
+	'attack' integer not null,
+	'defense' integer not null,
+	'hp' integer not null
+ );
+ 
+ create table 'pokemon_type' (
+ 	'id' integer not null,
+ 	'type_1' text not null,
+ 	'type_2' text default null,
+ 	foreign key ('id') references 'pokemon' ('id')
+ );
+ 
+create table 'effectiveness' (
+	'attack_type' text not null, 
+	'defense_type' text not null, 
+	'effect' float not null,
+	primary key ('attack_type', 'defense_type')
+);
+
+create table 'evolves' (
+	'id' integer not null,
+	'from_id' integer not null,
+	'to_id' integer not null,
+	foreign key ('id') references 'pokemon' ('id'),
+	foreign key ('from_id') references 'pokemon' ('id'),
+	foreign key ('to_id') references 'pokemon' ('id')
+);
+
+create table 'trainer' (
+	'id' integer primary key,
+	'name' integer not null
+);
+
+create table 'trainer_pokemon' (
+	'trainer_id' integer not null,
+	'pokemon_id' integer not null,
+	foreign key ('trainer_id') references 'trainer' ('id'),
+	foreign key ('pokemon_id') references 'pokemon' ('id')
+);
