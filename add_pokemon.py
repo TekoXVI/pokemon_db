1f6448ebe86fb4086c13962f1f87f96bc5450e36
Ian <tekoxvi@gmail.com>
2022-04-22 10:47:18 -0600

    add_pokemon

---
+++ add_pokemon.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import sys
+
+
+# ./add_pokemon.py trainer_id pokemon_id
+
+(trainer_id, pokemon_id) = sys.argv[1:3]
+
+con = sqlite3.connect('database.db')
+cur = con.cursor()
+
+cur.execute('INSERT INTO trainer_pokemon (trainer_id, pokemon_id) VALUES (?, ?)', [trainer_id, pokemon_id])
+
+con.commit()
+con.close()
