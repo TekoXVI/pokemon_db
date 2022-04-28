662ae26866377689712b21ca89a7c48563da432f
Wyatt Stevens <wyattstevens1995@gmail.com>
2022-04-28 15:56:01 -0600

    comparison now shows the pokemons types

--- compare.py
+++ compare.py
@@ -12,13 +12,22 @@ pokemon_name2 = sys.argv[2]
 con = sqlite3.connect('database.db')
 cur = con.cursor()
 
-cur.execute('SELECT * FROM pokemon WHERE name LIKE ?', [pokemon_name1])
-for row in cur:
-    print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]))
+#select * from pokemon A inner join pokemon_type B on A.id = B.id;
+#select * from pokemon A inner join pokemon_type B on A.id = B.id where A.id = ?;
 
-cur.execute('SELECT * FROM pokemon WHERE name LIKE ?', [pokemon_name2])
+cur.execute('select * from pokemon A inner join pokemon_type B on A.id = B.id where A.name = ?', [pokemon_name1])
 for row in cur:
-    print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]))
+    if row[7] == "":
+        print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]) + ", Type = " + row[6])
+    else:
+        print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]) + ", Type = " + row[6] + ", Type2 = " + row[7])
+
+cur.execute('select * from pokemon A inner join pokemon_type B on A.id = B.id where A.name = ?', [pokemon_name2])
+for row in cur:
+    if row[7] == "":
+        print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]) + ", Type = " + row[6])
+    else:
+        print(row[1] + ": " + "Attack = " + str(row[2]) + ", Defense = " + str(row[3]) + ", HP = " + str(row[4]) + ", Type = " + row[6] + ", Type2 = " + row[7])
 
 con.commit()
 con.close()
